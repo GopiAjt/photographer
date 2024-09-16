45d349e1c78c8450db833dc0f206d788ea595df1
GopiAjt <gopiajt23@gmail.com>
2024-09-16 22:31:02 +0530

    add: PhotographerController class

---
+++ src/main/java/com/capturenoww/photographer/controller/PhotographerController.java
@@ -0,0 +1,14 @@
+package com.capturenoww.photographer.controller;
+
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@Data
+@RequestMapping(path = "/photographer")
+public class PhotographerController {
+
+}
