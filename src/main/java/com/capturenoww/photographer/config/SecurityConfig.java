e9179ac106791b3d2656b443416ca2cd7b067c69
GopiAjt <gopiajt23@gmail.com>
2024-09-23 07:11:53 +0530

    add: add SecurityConfig for authentication and authorization

---
+++ src/main/java/com/capturenoww/photographer/config/SecurityConfig.java
@@ -0,0 +1,116 @@
+package com.capturenoww.photographer.config;
+
+
+import com.capturenoww.photographer.filter.JwtAuthFilter;
+import com.capturenoww.photographer.serviceimple.PhotographerUserDetailsService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Autowired
+	private JwtAuthFilter authFilter;
+
+
+	
+	@Bean
+	public UserDetailsService photographerDetailsService() {
+		return new PhotographerUserDetailsService();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.csrf(csrf -> csrf.disable())
+				.cors(withDefaults()) // Using Customizer.withDefaults() for CORS configuration
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/customer/signup", "/customer/authtoken", "/customer/validate",
+								"/photographer/signup", "/photographer/validate", "/photographer/signin",
+								"/photographer/authtoken", "/photographer/searchPhotographers",
+								"/photographer/forgotPassword", "/photographer/forgotPasswordOtp")
+						.permitAll() // Public endpoints
+						.requestMatchers("/photographer/add", "/photographer/deletePackage", "/photographer/getPackages",
+								"/photographer/updateBasicInfo", "/photographer/addpackage", "/photographer/getAlbums",
+								"/photographer/getEquipment", "/photographer/changePhoto", "/photographer/deletePhoto",
+								"/photographer/resetPasswordOtp", "/photographer/resetPassword", "/photographer/getReviews",
+								"/photographer/getBookingStatus", "/photographer/acceptDeclineBooking", "/photographer/addKycDetails")
+						.authenticated() // Authenticated endpoints
+				)
+				.sessionManagement(session -> session
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				)
+				.authenticationProvider(authenticationProviderP())
+				.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+
+	@Bean
+	public AuthenticationProvider authenticationProviderP() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(photographerDetailsService());
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		return authenticationProvider;
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Primary
+	@Qualifier("photographer")
+	@Bean
+	public AuthenticationManager authenticationManagerP(UserDetailsService userDetailsService) throws Exception {
+		var authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(photographerDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return new ProviderManager(authProvider);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource(HttpServletRequest request) {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		// Allow multiple origins
+		configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://capture-noww.vercel.app/", "http://127.0.0.1:5505"));
+
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+		// Allow credentials if needed
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+}
